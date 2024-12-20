68f33619f97003402be23ef245824cc0f29c6d0b
BelalElkhateeb20 <belalelmansy796@gmail.com>
2024-12-20 16:38:05 +0200

    newwwwww

---
+++ _prp/.ConsoleApp5/PhoneNums.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Xml.Linq;
+
+namespace ConsoleApp5.Prop_indexer
+{
+    public struct Phonebook
+    {
+        private long[] numbers;
+        private string[] names;
+        private int size;
+        public Phonebook( int Size)
+        {
+            this.size = Size;
+            numbers = new long[Size];
+            names = new string[Size];
+
+        }
+        public int _size { get { return size; } }
+        public void entryRange (int index,string name,long number)
+        {
+            if((index>=0)&&(index<=size))
+            {
+                numbers[index] = number;
+                names[index] = name;
+            }
+        }
+        public long GetNumber(string _name)
+        {
+            for (int i = 0; i < names?.Length; i++)
+            {
+                if (names[i] == _name)
+                    return numbers[i];
+            }
+            return -1;
+        }
+        public long this[string _name]
+        {
+            get
+            {
+                for (int i = 0; i < names?.Length; i++)
+                {
+                    if (names[i] == _name)
+                        return numbers[i];
+                }
+                return -1;
+            }
+            set
+            {
+                for (int i = 0; i < names?.Length; i++)
+                {
+                    if (names[i] == _name)
+                        numbers[i] = value;
+                }
+            }
+        }
+        public long this[long index, string _name]
+        {
+            set
+            {
+                if ((index >= 0) && (index <= size))
+                {
+                    numbers[index] = value;
+                    names[index] = _name;
+                }
+            }
+        }
+    }
+}
---
+++ _prp/.ConsoleApp5/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ConsoleApp5.Prop_indexer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Phonebook ob1=new Phonebook(5);
+            ob1.entryRange(0, "Abc", 010200000);
+            ob1.entryRange(1, "ADFR", 111188);
+            ob1.entryRange(2, "GTFD", 1880);
+
+            ob1["ADFR"] =1254;
+            ob1[1, "ADFR"] =1254;
+
+           
+            Console.WriteLine(ob1["GTFD"]);
+            //Console.WriteLine(ob1);
+            //ob1.entry(4, "n344rn",0100110);
+            //Console.WriteLine(ob1.GetNumber("Abc"));
+            Console.ReadLine(); 
+
+        }
+    }
+}
